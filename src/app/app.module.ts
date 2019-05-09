5d87965f44138fa4319fe650cae2795cc68d2b23
ElroyTsai <dada8269@gmail.com>
2019-05-09 14:48:15 +0800

    Layout -> header&Nav&footer Compontent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,22 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
+/**
+ * Layout
+ */
+import { HeaderComponent } from './Layout/header/header.component';
+import { NavComponent } from './Layout/nav/nav.component';
+import { FooterComponent } from './Layout/footer/footer.component';
+
 @NgModule({
-  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
